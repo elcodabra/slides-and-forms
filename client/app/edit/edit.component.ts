672bad7c4cdbe7d35f745066bf3f1a3fa0e73cf9
Alex Konovalov <akonovalov@at-consulting.ru>
2017-04-18 15:23:21 +0300

    upd edit component

--- client/app/edit/edit.component.ts
+++ client/app/edit/edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DataService } from "../services/data.service";
+import { ToastComponent } from "../shared/toast/toast.component";
 import 'rxjs/add/operator/switchMap';
+
 
 @Component({
   selector: 'app-edit',
@@ -12,6 +14,7 @@ export class EditComponent implements OnInit {
   private project: { name: string, content: string } = { name: '', content: '' };
 
   constructor(private route: ActivatedRoute,
+              public toast: ToastComponent,
               private dataService: DataService) { }
 
   ngOnInit() {
@@ -21,6 +24,21 @@ export class EditComponent implements OnInit {
         data => { this.project = data; this.project.content = JSON.stringify(data.content); },
         error => console.log("project error:", error),
       );
+  }
+
+  editProject(project) {
+    project.content = JSON.parse(project.content);
+    this.dataService.editProject(project).subscribe(
+      res => {
+        project.content = JSON.stringify(project.content);
+        this.project = project;
+        this.toast.setMessage('Project edited successfully.', 'success');
+      },
+      error => {
+        console.log(error)
+        this.toast.setMessage('error', 'error')
+      }
+    );
   }
 
 }
